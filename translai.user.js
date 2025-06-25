765433ea31e65da2ae73a94879f7d447825a818d
Dautsuro <cattydavid12@gmail.com>
2025-06-26 01:05:03 +0200

    fix(translai): when refusing to remove existing name it still added it now it does nothing

--- translai.user.js
+++ translai.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TranslAI
 // @namespace    https://github.com/Dautsuro/userscripts
-// @version      1.8.0
+// @version      1.8.1
 // @description  TranslAI auto-translates Chinese novel chapters to English with consistent names using a built-in NameManager.
 // @match        https://www.69shuba.com/book/*.htm
 // @match        https://www.69shuba.com/txt/*/*
@@ -391,6 +391,8 @@ class NameManager {
             if (confirm(`Erase previous name? (${existingName.translated})`)) {
                 Chapter.editName(existingName.translated, name.translated);
                 this.removeName(existingName);
+            } else {
+                return;
             }
         }
 
